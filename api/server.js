096ca284018df56a555d272bfb8bf53130733044
AlexTran0899 <76791231+AlexTran0899@users.noreply.github.com>
2021-10-19 15:48:12 -0500

    changed the server js file to mine

--- api/server.js
+++ api/server.js
@@ -1,37 +1,23 @@
-const express = require("express");
-const helmet = require("helmet");
-const cors = require("cors");
-const db = require("./data/db-config");
-const listingRouter = require("../api/Listings/listing-router");
+/* eslint-disable no-unused-vars */
+const express = require('express')
+const helmet = require('helmet')
+const cors = require('cors')
+const server = express()
+const Auth = require('./Auth/auth-router')
+const Listing = require('./Listings/listing-router')
 
-function getAllUsers() {
-  return db("users");
-}
+server.use(express.json())
+server.use(helmet())
+server.use(cors())
 
-async function insertUser(user) {
-  // WITH POSTGRES WE CAN PASS A "RETURNING ARRAY" AS 2ND ARGUMENT TO knex.insert/update
-  // AND OBTAIN WHATEVER COLUMNS WE NEED FROM THE NEWLY CREATED/UPDATED RECORD
-  // UNLIKE SQLITE WHICH FORCES US DO DO A 2ND DB CALL
-  const [newUserObject] = await db("users").insert(user, [
-    "user_id",
-    "username",
-    "password",
-  ]);
-  return newUserObject; // { user_id: 7, username: 'foo', password: 'xxxxxxx' }
-}
+server.use('/api/auth', Auth)
+server.use('/api/Listing', Listing)  
 
-const server = express();
-server.use(express.json());
-server.use(helmet());
-server.use(cors());
-server.use("/api/listings", listingRouter);
-
-server.get("/api/users", async (req, res) => {
-  res.json(await getAllUsers());
-});
-
-server.post("/api/users", async (req, res) => {
-  res.status(201).json(await insertUser(req.body));
-});
+server.use((err, req, res, next) => {
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack
+    })
+})
 
 module.exports = server;
