099d49a7eff1d824abbb0f4c5e3e555f8af35fb3
Matthew Bokovitz <MattBokovitz@Matthews-MBP-2.fios-router.home>
2021-10-20 16:24:21 -0400

    fixed user database structure

--- api/server.js
+++ api/server.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const server = express();
 const Auth = require("./Auth/auth-router");
 const Listing = require("./Listings/listing-router");
+const UploadImage = require("./upload_image/upload_image");
 
 server.use(express.json());
 server.use(helmet());
@@ -12,6 +13,7 @@ server.use(cors());
 
 server.use("/api/auth", Auth);
 server.use("/api/listing", Listing);
+server.use("/api/uploadimage", UploadImage);
 
 server.use((err, req, res, next) => {
   res.status(500).json({
---
+++ api/upload_image/upload_image-model.js
@@ -0,0 +1,27 @@
+const db = require("../data/db-config");
+
+function Add(data) {
+  // done for you
+  return db("image").insert(data, ["*"]);
+}
+function update(data) {
+  // done for you
+  return db("image").where({ image_id: data.image_id }).update(data, ["*"]);
+}
+
+function original_image(image_key, data) {
+  // done for you
+  return db("image").where({ image_key }).update(data);
+}
+
+function addingTags(url, data) {
+  // done for you
+  return db("image").where({ url }).update({ tags: data }, ["*"]);
+}
+
+module.exports = {
+  Add,
+  update,
+  addingTags,
+  original_image,
+};
